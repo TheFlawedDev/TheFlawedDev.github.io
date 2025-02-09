b2e78086ce63a35bd3e563437b38d77c098c000f
vela <97405904+TheFlawedDev@users.noreply.github.com>
2025-02-08 19:04:42 -0700

    mdn web docs -'Your first website' Article

---
+++ js/main.js
@@ -0,0 +1,27 @@
+const myImage = document.querySelector("img");
+
+myImage.addEventListener("click", function () {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "img/riri.jpg") {
+    myImage.setAttribute("src", "img/riri2.jpg");
+  } else {
+    myImage.setAttribute("src", "img/riri.jpg");
+  }
+});
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1")
+
+function setUserName() {
+  const myName = prompt("Please enter your name.");
+  if (!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem("name", myName);
+    myHeading.textContent = `Ari is cooler than, ${myName}`;
+  }
+}
+
+myButton.addEventListener("click", () => {
+  setUserName();
+});
